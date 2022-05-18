33e9722ed3285c5ef8a67d4851e5f090a92876b8
NikolayVVV <tentyuk95@mail.ru>
2022-05-18 09:41:39 +0300

    Client

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -1,6 +1,8 @@
 package ru.itsjava;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -9,12 +11,17 @@ public class Main {
     public final static String HOST = "localhost";
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(HOST,PORT);
+        Socket socket = new Socket(HOST, PORT);
 
-        if (socket.isConnected()){
+        if (socket.isConnected()) {
             PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
             serverWriter.println("Hi, from client");
             serverWriter.flush();
+
+            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (true) {
+                System.out.println(serverReader.readLine());
+            }
         }
     }
 }
